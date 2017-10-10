cc40dd27a022d1ad272fc81a023d8e54f2a3feda
Muhammad Ansab Iqbal <ansab.iqbal@ba.com>
2017-10-10 11:29:36 +0500

    Initial

---
+++ TimeZonesDemoForCampaigns/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeZonesDemoForCampaigns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadOnlyCollection<TimeZoneInfo> timeZones = TimeZoneInfo.GetSystemTimeZones();
+
+            foreach (TimeZoneInfo timeZoneInfo in timeZones)
+                Console.WriteLine("Timezone : " + timeZoneInfo.DisplayName + "\nDifference : " + timeZoneInfo.BaseUtcOffset + "\n\n");
+
+
+            Console.ReadKey();
+        }
+    }
+}
